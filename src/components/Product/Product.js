010c03a584c2d584ef2f50b74dbfb06754a6978a
dulal <dulalmiah70112@gmail.com>
2023-01-13 18:37:42 +0600

    products section update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,26 +2,35 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Product.css';
 
-const Product = ({product}) => {
-    const {name , category , rentPrice , price , img,seller}  = product;
+const Product = ({ product }) => {
 
+
+    const { name, rentPrice, price, img, seller } = product;
+
+    //react-router hook
     const navigate = useNavigate();
 
-    const productInfo = (product) =>{
+    const productInfo = (product) => {
 
-       const path = `/productInformation/${product.id}`;
-       navigate(path);
+        const path = `/productInformation/${product.id}`;
+        navigate(path);
 
     }
 
     return (
-        <div className='product-container   my-5   p-5 col-md-4 col-sm-12' onClick={()=>productInfo(product)}>
-            <h3>{name}</h3>
-            <img src={img} alt="" height='100' width='100' className='mx-auto'/>
-            <p>Category: { category}</p>
-            <p><small>Rent Price:{rentPrice}</small></p>
-            <p><small>Price:{price}</small></p>
-            <p><small>Seller:{seller}</small></p>
+        <div className='product-container my-5  p-5 col-md-4 col-sm-12'>
+
+            <div className='image-div'>
+                <img src={img} alt="" height='100' width='100' className='mx-auto' />
+            </div>
+
+            <div className='m-3 information-div'>
+                <h3>{name}</h3>
+                <h5><small>Rent Price:{rentPrice}</small></h5>
+                <p><small>Price:{price}</small></p>
+                <p>Seller:{seller}</p>
+                <button className='btn btn-danger' onClick={() => productInfo(product)}>Click for Information</button>
+            </div>
         </div>
     );
 };
